9e581daa81fadc647170ea4ac7ceae1b0d13995f
Aleksandar <44511298+renesansa1@users.noreply.github.com>
2019-11-27 13:04:34 +0100

    V 1.0

--- login_submit.php
+++ login_submit.php
@@ -22,7 +22,7 @@
 <meta http-equiv="refresh" content="1;url=login.php" />
 <?php
         //header('location: login');
-        //echo "Wrong email or password.";
+        //echo "Pogresan email ili sifra.";
     } else {
         $row = mysqli_fetch_array($user_authentication_result);
         $_SESSION['email'] = $email;
